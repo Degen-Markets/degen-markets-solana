b797319630b117a2293b6245bfa32205ef35a42b
Angad Singh <angad17@hotmail.com>
2024-07-13 15:18:34 +0100

    Add test to ensure that poolOption title matches hash

--- tests/createOption.test.ts
+++ tests/createOption.test.ts
@@ -14,7 +14,7 @@ describe("Option Creation", () => {
     anchor.setProvider(provider);
     const program = anchor.workspace.DegenPools as anchor.Program<DegenPools>;
 
-    it('should succeed if hash is correct', async () => {
+    it('should succeed if hash is correct & fails with custom error if incorrect', async () => {
         const authorityKeypair = await getLocalAccount();
         const title = "Who will win the 2024 Euros?";
         const poolAccountKey = await derivePoolAccountKey(title);
@@ -39,5 +39,21 @@ describe("Option Creation", () => {
             .rpc();
         const account = await program.account.poolOption.fetch(optionAccountKey);
         expect(account.title).to.eql(optionTitle);
+
+        const optionTwo = "Spain";
+        const optionTwoAccountKey = await deriveOptionAccountKey(optionTwo, poolAccountKey);
+        try {
+            await program.methods.createOption("randomText", getOptionTitleHash(poolAccountKey, optionTwo) as unknown as number[])
+                .accounts({
+                    optionAccount: optionTwoAccountKey,
+                    poolAccount: poolAccountKey,
+                    wallet: authorityKeypair.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                })
+                .signers([authorityKeypair])
+                .rpc();
+        } catch (e) {
+            expect(e.message).to.include("PoolOptionDoesNotMatchHash");
+        }
     });
 });
